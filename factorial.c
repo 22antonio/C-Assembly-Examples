dc671d97b9039201fa649f285e626d4660fad03a
Tony <antonio.medel2021@email.bakersfieldcollege.edu>
2017-10-24 18:28:44 -0700

    Add files via upload
    Demonstrates factorial in c

---
+++ factorial.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int fact(int n)
+{
+    if (n <= 0)
+        return 1;
+    else
+        return n * fact(n - 1);
+}
+
+int main(void)
+{
+    int value = 0;
+    int result = 0;
+
+    printf("Please enter an integer value: ");
+    scanf("%d",&value);
+
+    result = fact(value);
+    printf("Your factorial of %d is %d\n",value, result);
+
+}
